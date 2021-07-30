a7ff627859ed4b5073e68c7374cd058714e4862c
Fabio Murer <murer.fabio23@gmail.com>
2021-07-30 12:40:47 +0200

    sss

--- code/work_in_progress/zwdata.c
+++ code/work_in_progress/zwdata.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define	MAXDIGITS   20     /* maximum length zwdata */ 
-#define PLUS        1       /* positive sign bit */
-#define MINUS       -1      /* negative sign bit */
+#define MAXDIGITS 20 /* maximum length zwdata */
+#define PLUS 1       /* positive sign bit */
+#define MINUS -1     /* negative sign bit */
+#define ISSTRING 2   /* istring  sing bit */
 
-typedef struct {
+typedef struct
+{
     char digits[MAXDIGITS]; /* represent the number */
-    int signbit;            /* 1 if positive, -1 if negative */ 
-    int length;          /* number of digit in zwdata */
+    int signbit;            /* 1 if positive, -1 if negative */
+    int length;             /* number of digit in zwdata */
 } zwdata;
 
 void print_zwdata(zwdata *n);
@@ -25,116 +28,148 @@ void divide_zwdata(zwdata *a, zwdata *b, zwdata *c);
 
 void add_zwdata(zwdata *a, zwdata *b, zwdata *c);
 
-void print_zwdata(zwdata *n) {
+void print_zwdata(zwdata *n)
+{
     int i;
 
-    if (n->signbit == MINUS) {
+    if (n->signbit == MINUS)
+    {
         printf("- ");
     }
 
-    for (i = n->length; i >= 0; i--) {
-        printf("%c",'0'+ n->digits[i]);
+    for (i = n->length; i >= 0; i--)
+    {
+        printf("%c", '0' + n->digits[i]);
     }
-
-    printf("\n");
 }
 
-void zero_justify(zwdata *n) {
-    while ((n->length > 0) && (n->digits[ n->length ] == 0)) {
+void zero_justify(zwdata *n)
+{
+    while ((n->length > 0) && (n->digits[n->length] == 0))
+    {
         n->length--;
     }
 
-    if ((n->length == 0) && (n->digits[0] == 0)) {
-        n->signbit = PLUS;	/* hack to avoid -0 */
+    if ((n->length == 0) && (n->digits[0] == 0))
+    {
+        n->signbit = PLUS; /* hack to avoid -0 */
     }
 }
 
-void int_to_zwdata(int s, zwdata *n) {
-    int i;    /* counter */
-    int t;    /* int to work with */
+void int_to_zwdata(int s, zwdata *n)
+{
+    int i; /* counter */
+    int t; /* int to work with */
 
-    if (s >= 0) {
+    if (s >= 0)
+    {
         n->signbit = PLUS;
-    } else {
+    }
+    else
+    {
         n->signbit = MINUS;
     }
 
-    for (i = 0; i < MAXDIGITS; i++) {
-        n->digits[i] = (char) 0;
+    for (i = 0; i < MAXDIGITS; i++)
+    {
+        n->digits[i] = (char)0;
     }
 
     n->length = -1;
 
     t = abs(s);
 
-    while (t > 0) {
-        n->length ++;
+    while (t > 0)
+    {
+        n->length++;
         n->digits[n->length] = (t % 10);
         t = t / 10;
-	}
+    }
 
-    if (s == 0) {
+    if (s == 0)
+    {
         n->length = 0;
     }
 }
 
-void initialize_zwdata(zwdata *n) {
+void string_to_zwdata(char *string, zwdata *n)
+{
+    n->signbit = ISSTRING;
+    n->length = strlen(string) + 1; //non conta in \0 finale quindi real legnt++
+    strncmp(n->digits, string, MAXDIGITS); //copy the first MAXDIGITS char so not overflow
+}
+
+void initialize_zwdata(zwdata *n)
+{
     int_to_zwdata(0, n);
 }
 
-int max(int a, int b) {
-    if (a > b) {
-        return(a);
+int max(int a, int b)
+{
+    if (a > b)
+    {
+        return (a);
     }
-    return(b);
+    return (b);
 }
 
-int compare_zwdata(zwdata *a, zwdata *b) {
-    int i;    /* counter */
+int compare_zwdata(zwdata *a, zwdata *b)
+{
+    int i; /* counter */
 
-    if ((a->signbit == MINUS) && (b->signbit == PLUS)) {
-        return(PLUS);
+    if ((a->signbit == MINUS) && (b->signbit == PLUS))
+    {
+        return (PLUS);
     }
 
-    if ((a->signbit == PLUS) && (b->signbit == MINUS)) {
-        return(MINUS);
+    if ((a->signbit == PLUS) && (b->signbit == MINUS))
+    {
+        return (MINUS);
     }
 
-    if (b->length > a->length) {
+    if (b->length > a->length)
+    {
         return (PLUS * a->signbit);
     }
 
-    if (a->length > b->length) {
+    if (a->length > b->length)
+    {
         return (MINUS * a->signbit);
     }
 
-    for (i = a->length; i >= 0; i--) {
-        if (a->digits[i] > b->digits[i]) {
-            return(MINUS * a->signbit);
+    for (i = a->length; i >= 0; i--)
+    {
+        if (a->digits[i] > b->digits[i])
+        {
+            return (MINUS * a->signbit);
         }
-        if (b->digits[i] > a->digits[i]) {
-            return(PLUS * a->signbit);
+        if (b->digits[i] > a->digits[i])
+        {
+            return (PLUS * a->signbit);
         }
-	}
+    }
 
-    return(0);
+    return (0);
 }
 
-void subtract_zwdata(zwdata *a, zwdata *b, zwdata *c) {
-    int borrow;         /* has anything been borrowed? */
-    int v;              /* placeholder digit */
-    int i;              /* counter */
+void subtract_zwdata(zwdata *a, zwdata *b, zwdata *c)
+{
+    int borrow; /* has anything been borrowed? */
+    int v;      /* placeholder digit */
+    int i;      /* counter */
 
     initialize_zwdata(c);
 
-    if ((a->signbit == MINUS) || (b->signbit == MINUS)) {
+    if ((a->signbit == MINUS) || (b->signbit == MINUS))
+    {
         b->signbit = -1 * b->signbit;
         add_zwdata(a, b, c);
         b->signbit = -1 * b->signbit;
         return;
     }
 
-    if (compare_zwdata(a, b) == PLUS) {
+    if (compare_zwdata(a, b) == PLUS)
+    {
         subtract_zwdata(b, a, c);
         c->signbit = MINUS;
         return;
@@ -143,16 +178,19 @@ void subtract_zwdata(zwdata *a, zwdata *b, zwdata *c) {
     c->length = max(a->length, b->length);
     borrow = 0;
 
-    for (i = 0; i <= (c->length); i++) {
+    for (i = 0; i <= (c->length); i++)
+    {
         v = (a->digits[i] - borrow - b->digits[i]);
-        if (a->digits[i] > 0) {
+        if (a->digits[i] > 0)
+        {
             borrow = 0;
         }
-        if (v < 0) {
+        if (v < 0)
+        {
             v = v + 10;
             borrow = 1;
         }
-        c->digits[i] = (char) v % 10;
+        c->digits[i] = (char)v % 10;
     }
 
     zero_justify(c);
@@ -160,20 +198,27 @@ void subtract_zwdata(zwdata *a, zwdata *b, zwdata *c) {
 
 /*	c = a +- b;	*/
 
-void add_zwdata(zwdata *a, zwdata *b, zwdata *c) {
-    int carry;          /* carry digit */
-    int i;              /* counter */
+void add_zwdata(zwdata *a, zwdata *b, zwdata *c)
+{
+    int carry; /* carry digit */
+    int i;     /* counter */
 
     initialize_zwdata(c);
 
-    if (a->signbit == b->signbit) {
+    if (a->signbit == b->signbit)
+    {
         c->signbit = a->signbit;
-    } else {
-        if (a->signbit == MINUS) {
+    }
+    else
+    {
+        if (a->signbit == MINUS)
+        {
             a->signbit = PLUS;
             subtract_zwdata(b, a, c);
             a->signbit = MINUS;
-        } else {
+        }
+        else
+        {
             b->signbit = PLUS;
             subtract_zwdata(a, b, c);
             b->signbit = MINUS;
@@ -184,8 +229,9 @@ void add_zwdata(zwdata *a, zwdata *b, zwdata *c) {
     c->length = max(a->length, b->length) + 1;
     carry = 0;
 
-    for (i = 0; i <= (c->length); i++) {
-        c->digits[i] = (char) (carry + a->digits[i] + b->digits[i]) % 10;
+    for (i = 0; i <= (c->length); i++)
+    {
+        c->digits[i] = (char)(carry + a->digits[i] + b->digits[i]) % 10;
         carry = (carry + a->digits[i] + b->digits[i]) / 10;
     }
 
@@ -194,35 +240,42 @@ void add_zwdata(zwdata *a, zwdata *b, zwdata *c) {
 
 /* multiply n by 10^d */
 
-void digit_shift(zwdata *n, int d) {
-    int i;    /* counter */
+void digit_shift(zwdata *n, int d)
+{
+    int i; /* counter */
 
-    if ((n->length == 0) && (n->digits[0] == 0)) {
+    if ((n->length == 0) && (n->digits[0] == 0))
+    {
         return;
     }
 
-    for (i = n->length; i >= 0; i--) {
+    for (i = n->length; i >= 0; i--)
+    {
         n->digits[i + d] = n->digits[i];
     }
 
-    for (i = 0; i < d; i++) {
+    for (i = 0; i < d; i++)
+    {
         n->digits[i] = 0;
     }
 
     n->length = n->length + d;
 }
 
-void multiply_zwdata(zwdata *a, zwdata *b, zwdata *c) {
-    zwdata row;         /* represent shifted row */
-    zwdata tmp;         /* placeholder zwdata */
-    int i, j;           /* counters */
+void multiply_zwdata(zwdata *a, zwdata *b, zwdata *c)
+{
+    zwdata row; /* represent shifted row */
+    zwdata tmp; /* placeholder zwdata */
+    int i, j;   /* counters */
 
     initialize_zwdata(c);
 
     row = *a;
 
-    for (i = 0; i <= b->length; i++) {
-        for (j = 1; j <= b->digits[i]; j++) {
+    for (i = 0; i <= b->length; i++)
+    {
+        for (j = 1; j <= b->digits[i]; j++)
+        {
             add_zwdata(c, &row, &tmp);
             *c = tmp;
         }
@@ -234,11 +287,12 @@ void multiply_zwdata(zwdata *a, zwdata *b, zwdata *c) {
     zero_justify(c);
 }
 
-void divide_zwdata(zwdata *a, zwdata *b, zwdata *c) {
-    zwdata row;                     /* represent shifted row */
-    zwdata tmp;                     /* placeholder zwdata */
-    int asign, bsign;               /* temporary signs */
-    int i;                          /* counters */
+void divide_zwdata(zwdata *a, zwdata *b, zwdata *c)
+{
+    zwdata row;       /* represent shifted row */
+    zwdata tmp;       /* placeholder zwdata */
+    int asign, bsign; /* temporary signs */
+    int i;            /* counters */
 
     initialize_zwdata(c);
 
@@ -255,12 +309,14 @@ void divide_zwdata(zwdata *a, zwdata *b, zwdata *c) {
 
     c->length = a->length;
 
-    for (i = a->length; i >= 0; i--) {
+    for (i = a->length; i >= 0; i--)
+    {
         digit_shift(&row, 1);
         row.digits[0] = a->digits[i];
         c->digits[i] = 0;
-        while (compare_zwdata(&row, b) != PLUS) {
-            c->digits[i] ++;
+        while (compare_zwdata(&row, b) != PLUS)
+        {
+            c->digits[i]++;
             subtract_zwdata(&row, b, &tmp);
             row = tmp;
         }
@@ -272,63 +328,97 @@ void divide_zwdata(zwdata *a, zwdata *b, zwdata *c) {
     b->signbit = bsign;
 }
 
-int main(void) {
-    int a,b;
+int main(void)
+{
+    int a, b;
     zwdata n1, n2, n3, zero;
     zwdata arr_zwdata[10];
 
-    for (int i=0; i<10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         initialize_zwdata(&arr_zwdata[i]);
     }
-    
+
     char command;
     int arg1, arg2, arg3;
-    
-    while(1) {
+
+    while (1)
+    {
         printf("enter command ");
-        scanf(" %c",&command);
+        scanf(" %c", &command);
         if (command == 'Q')
         {
             break;
         }
 
         printf("enter arg1\n");
-        scanf("%d",&arg1);
+        scanf("%d", &arg1);
         printf("enter arg2\n");
-        scanf("%d",&arg2);
+        scanf("%d", &arg2);
         printf("enter arg3\n");
-        scanf("%d",&arg3);
+        scanf("%d", &arg3);
 
         switch (command)
         {
         case 'i':
+            printf("enter arg1\n");
+            scanf("%d", &arg1);
+            printf("enter arg2\n");
+            scanf("%d", &arg2);
             int_to_zwdata(arg1, &arr_zwdata[arg2]);
             break;
 
         case 'a':
+            printf("enter arg1\n");
+            scanf("%d", &arg1);
+            printf("enter arg2\n");
+            scanf("%d", &arg2);
+            printf("enter arg3\n");
+            scanf("%d", &arg3);
             add_zwdata(&arr_zwdata[arg1], &arr_zwdata[arg2], &arr_zwdata[arg3]);
             break;
-        
+
         case 's':
+            printf("enter arg1\n");
+            scanf("%d", &arg1);
+            printf("enter arg2\n");
+            scanf("%d", &arg2);
+            printf("enter arg3\n");
+            scanf("%d", &arg3);
             subtract_zwdata(&arr_zwdata[arg1], &arr_zwdata[arg2], &arr_zwdata[arg3]);
             break;
-        
-        case 'm':
+
+        case 'm': //multiply
+            printf("enter arg1\n");
+            scanf("%d", &arg1);
+            printf("enter arg2\n");
+            scanf("%d", &arg2);
+            printf("enter arg3\n");
+            scanf("%d", &arg3);
             multiply_zwdata(&arr_zwdata[arg1], &arr_zwdata[arg2], &arr_zwdata[arg3]);
             break;
-        
-        case 'd':
+
+        case 'd': //divide
+            printf("enter arg1\n");
+            scanf("%d", &arg1);
+            printf("enter arg2\n");
+            scanf("%d", &arg2);
+            printf("enter arg3\n");
+            scanf("%d", &arg3);
             divide_zwdata(&arr_zwdata[arg1], &arr_zwdata[arg2], &arr_zwdata[arg3]);
             break;
-        
+
         case 'p':
+            printf("enter arg1\n");
+            scanf("%d", &arg1);
             print_zwdata(&arr_zwdata[arg1]);
 
         default:
             break;
         }
 
-        for (int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             printf("arr_zwdata[%d] = ", i);
             print_zwdata(&arr_zwdata[i]);
             printf("\n");
