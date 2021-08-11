3088189e2931ecc285885d6b8424f38e62172bc9
ZwirgioIlredelCodice <murer.fabio23@gmail.com>
2021-08-11 21:18:16 +0200

    testvm some problems

--- code/lelloVM/testvm.h
+++ code/lelloVM/testvm.h
@@ -6,6 +6,9 @@
 
 void test_vm()
 {
+	
+	inizzializza_memoria(50);
+	
 	int test_carica[] = {
 						carica, 666, 0, 0,
 						fine, 0, 0, 0
@@ -60,4 +63,158 @@ void test_vm()
 	printf("dividi expected 2 output ");
 	SCRIVI(5);
 	printf("/n");
+	
+	int test_e[] = 		{
+						carica, 1, 0, 0,
+						carica, 1, 1, 0,
+						e, 0, 1, 6,
+						fine, 0, 0, 0
+						};
+	esegui(test_e);
+	printf("e expected 1 output ");
+	SCRIVI(6);
+	printf("/n");
+	
+	int test_o[] = 		{
+						carica, 1, 0, 0,
+						carica, 0, 1, 0,
+						o, 0, 1, 7,
+						fine, 0, 0, 0
+						};
+	esegui(test_o);
+	printf("o expected 1 output ");
+	SCRIVI(7);
+	printf("/n");
+	
+	int test_non[] = 		{
+						carica, 1, 0, 0,
+						non, 0, 8, 0,
+						fine, 0, 0, 0
+						};
+	esegui(test_non);
+	printf("non expected 0 output ");
+	SCRIVI(8);
+	printf("/n");
+	
+	int test_uguale[] = 		{
+						carica, 1, 0, 0,
+						carica, 1, 1, 0,
+						uguale, 0, 1, 9,
+						fine, 0, 0, 0
+						};
+	esegui(test_uguale);
+	printf("uguale expected 1 output ");
+	SCRIVI(9);
+	printf("/n");
+	
+	int test_nonuguale[] = 		{
+						carica, 1, 0, 0,
+						carica, 1, 1, 0,
+						nonuguale, 0, 1, 10,
+						fine, 0, 0, 0
+						};
+	esegui(test_nonuguale);
+	printf("nonuguale expected 0 output ");
+	SCRIVI(10);
+	printf("/n");
+	
+	int test_minore[] = 		{
+						carica, 2, 0, 0,
+						carica, 1, 1, 0,
+						minore, 0, 1, 11,
+						fine, 0, 0, 0
+						};
+	esegui(test_minore);
+	printf("minore expected 0 output ");
+	SCRIVI(11);
+	printf("/n");
+	
+	int test_maggiore[] = 		{
+						carica, 2, 0, 0,
+						carica, 1, 1, 0,
+						maggiore, 0, 1, 12,
+						fine, 0, 0, 0
+						};
+	esegui(test_maggiore);
+	printf("maggiore expected 1 output ");
+	SCRIVI(12);
+	printf("/n");
+	
+	int test_minoreuguale[] = 		{
+						carica, 2, 0, 0,
+						carica, 2, 1, 0,
+						minoreuguale, 0, 1, 12,
+						fine, 0, 0, 0
+						};
+	esegui(test_minoreuguale);
+	printf("minoreuguale expected 1 output ");
+	SCRIVI(13);
+	printf("/n");
+	
+	int test_maggioreuguale[] = 		{
+						carica, 2, 0, 0,
+						carica, 2, 1, 0,
+						maggioreuguale, 0, 1, 13,
+						fine, 0, 0, 0
+						};
+	esegui(test_maggioreuguale);
+	printf("maggioreuguale expected 1 output ");
+	SCRIVI(13);
+	printf("/n");
+	
+	
+	int test_vai[] = 		{
+						vai, 3 , 0, 0,
+						fine, 0, 0, 0,
+						carica, 8, 14, 0,
+						fine, 0, 0, 0
+						};
+	esegui(test_vai);
+	printf("vai expected 8 output ");
+	SCRIVI(14);
+	printf("/n");
+	
+	int test_vai_vero[] = 		{
+						carica, 1, 0, 0,
+						vai_vero, 0 , 4, 0,
+						fine, 0, 0, 0,
+						carica, 8, 15, 0,
+						fine, 0, 0, 0
+						};
+	esegui(test_vai_vero);
+	printf("vai_vero expected 8 output ");
+	SCRIVI(15);
+	printf("/n");
+	
+	int test_vai_falso[] = 		{
+						carica, 0, 0, 0,
+						vai_falso, 0 , 4, 0,
+						fine, 0, 0, 0,
+						carica, 8, 16, 0,
+						fine, 0, 0, 0
+						};
+	esegui(test_vai_falso);
+	printf("vai_falso expected 8 output ");
+	SCRIVI(16);
+	printf("/n");
+	
+	int test_scrivi[] = 		{
+						carica, 11, 0, 0,
+						scrivi, 0, 0, 0,
+						fine, 0, 0, 0
+						};
+	esegui(test_scrivi);
+	printf("scrivi expected 11 output ");
+	printf("/n");
+	
+	printf("immetti quello che vuoi\n");
+	int test_immetti[] = 		{
+						immetti, 0, 0, 0,
+						fine, 0, 0, 0
+						};
+	esegui(test_immetti);
+	printf("immetti expected cosa ai messo? output ");
+	SCRIVI(0);
+	printf("/n");
+	
 }
--- code/main.c
+++ code/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lelloVM/vm.h"
 #include "lelloASSEBLY/lelloBLER.h"
+#include "lelloVM/testvm.h"
 
 int main(int argc, char *argv[])
 { //per command line argument
@@ -10,13 +12,67 @@ int main(int argc, char *argv[])
 	
     if (argc == 2)
     {
-        FILE *fp = fopen(argv[1], "r");
-        // Check if file exists
-        if (fp == NULL)
-        {
-            printf("file %s non esistente [ERRORE]\n", argv[1]);
-            return -1;
-        }
+		if (strcmp("runtest", argv[1]) == 0)
+		{
+			test_vm();
+		}
+		else
+		{
+       		FILE *fp = fopen(argv[1], "r");
+        	// Check if file exists
+        	if (fp == NULL)
+        	{
+            	printf("file %s non esistente [ERRORE]\n", argv[1]);
+            	return -1;
+        	}
+			else
+			{
+			    printf("eseguzione file %s\n", argv[1]);
+
+    			int numero_righe = line_number(argv[1]) + 1;
+    			int numero_comandi = numero_righe * 4;
+
+    			int *comandi_array;
+    			comandi_array = calloc(numero_comandi, sizeof(int)); //crea un array di int per tnenere tutti i comandi
+    			if (comandi_array == NULL)
+    			{
+        			printf("non riesco a creare array comandi_array [ERRORE]\n");
+        			return 1;
+    			}
+
+    			error = parse_lelloBLER(argv[1], comandi_array);
+	
+				//debug programma
+				printf("\noutput programma:\n");
+    			for (int i = 0; i < numero_comandi; i++)
+    			{
+        			printf("%d ", comandi_array[i]);
+    			}
+	
+				if (error > 0)
+				{
+					printf("ERROR at parse_lellBLER, exit...\n");
+					return 1;
+				}
+
+    			/*
+    			---------ESEGUZIONE
+    			*/
+    			printf("\noutput esecuzione:\n");
+    			inizzializza_memoria(50);
+    			esegui(comandi_array);
+	
+    			//debug memoria
+    			printf("\nmemoria\n");
+    			for (int i = 0; i < grandezza_memoria; i++)
+    			{
+        			print_zwdata(&mem[i]);
+					printf("\t");
+    			}
+    
+    			printf("\n");	
+			}
+		}
     }
     else if (argc > 2)
     {
@@ -28,49 +84,4 @@ int main(int argc, char *argv[])
         printf("immetti il nome del file da eseguire [ERRORE]\n");
         return -1;
     }
-
-    printf("eseguzione file %s\n", argv[1]);
-
-    int numero_righe = line_number(argv[1]) + 1;
-    int numero_comandi = numero_righe * 4;
-
-    int *comandi_array;
-    comandi_array = calloc(numero_comandi, sizeof(int)); //crea un array di int per tnenere tutti i comandi
-    if (comandi_array == NULL)
-    {
-        printf("non riesco a creare array comandi_array [ERRORE]\n");
-        return 1;
-    }
-
-    error = parse_lelloBLER(argv[1], comandi_array);
-	
-	//debug programma
-	printf("\noutput programma:\n");
-    for (int i = 0; i < numero_comandi; i++)
-    {
-        printf("%d ", comandi_array[i]);
-    }
-	
-	if (error > 0)
-	{
-		printf("ERROR at parse_lellBLER, exit...\n");
-		return 1;
-	}
-
-    /*
-    ---------ESEGUZIONE
-    */
-    printf("\noutput esecuzione:\n");
-    inizzializza_memoria(50);
-    esegui(comandi_array);
-	
-    //debug memoria
-    printf("\nmemoria\n");
-    for (int i = 0; i < grandezza_memoria; i++)
-    {
-        print_zwdata(&mem[i]);
-		printf("\t");
-    }
-    
-    printf("\n");
 }
